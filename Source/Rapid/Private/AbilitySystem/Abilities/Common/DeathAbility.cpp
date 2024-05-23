aadcf4e3e0f6f3b50455e8287643d935af96f001
apokrif6 <j.berenchan@gmail.com>
2024-05-23 23:47:10 +0200

    Add first iteration of DeathAbility

---
+++ Source/Rapid/Private/AbilitySystem/Abilities/Common/DeathAbility.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/Common/DeathAbility.h"
+#include "GameFramework/MovementComponent.h"
+
+void UDeathAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+                                    const FGameplayAbilityActivationInfo ActivationInfo,
+                                    const FGameplayEventData* TriggerEventData)
+{
+	if (USkeletalMeshComponent* AvatarSkeletalMeshComponent = ActorInfo->SkeletalMeshComponent.Get())
+	{
+		AvatarSkeletalMeshComponent->SetCollisionProfileName(RagdollProfileName);
+		AvatarSkeletalMeshComponent->SetSimulatePhysics(true);
+	}
+
+	if (UMovementComponent* AvatarMovementComponent = ActorInfo->MovementComponent.Get())
+	{
+		AvatarMovementComponent->StopMovementImmediately();
+	}
+
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
---
+++ Source/Rapid/Public/AbilitySystem/Abilities/Common/DeathAbility.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "DeathAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAPID_API UDeathAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	                             const FGameplayAbilityActivationInfo ActivationInfo,
+	                             const FGameplayEventData* TriggerEventData) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Config")
+	FName RagdollProfileName = "Ragdoll";
+};
