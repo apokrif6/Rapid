89af4ccb25e9357e7931f2ca211e6ec1b8ea9f80
apokrif6 <j.berenchan@gmail.com>
2024-05-26 22:10:46 +0200

    Implement Death ability for BaseEnemy

--- Source/Rapid/Private/AbilitySystem/Abilities/Common/DeathAbility.cpp
+++ Source/Rapid/Private/AbilitySystem/Abilities/Common/DeathAbility.cpp
@@ -2,12 +2,20 @@
 
 
 #include "AbilitySystem/Abilities/Common/DeathAbility.h"
+#include "AbilitySystemComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Character.h"
 #include "GameFramework/MovementComponent.h"
 
 void UDeathAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
                                     const FGameplayAbilityActivationInfo ActivationInfo,
                                     const FGameplayEventData* TriggerEventData)
 {
+	if (const ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get()))
+	{
+		Character->GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
+
 	if (USkeletalMeshComponent* AvatarSkeletalMeshComponent = ActorInfo->SkeletalMeshComponent.Get())
 	{
 		AvatarSkeletalMeshComponent->SetCollisionProfileName(RagdollProfileName);
