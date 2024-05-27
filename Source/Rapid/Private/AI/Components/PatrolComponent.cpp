e9b546e297a97da2eb2fae7a5a535a5d810fc7ce
apokrif6 <j.berenchan@gmail.com>
2024-05-27 22:55:33 +0200

    Start working on PatrolComponent

---
+++ Source/Rapid/Private/AI/Components/PatrolComponent.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Components/PatrolComponent.h"
+
+UPatrolComponent::UPatrolComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UPatrolComponent::SetPointToNext()
+{
+	CurrentPointIndex = (CurrentPointIndex + 1) % Points.Num();
+}
+
+TSoftObjectPtr<USceneComponent> UPatrolComponent::GetCurrentPoint() const
+{
+	return Points.IsValidIndex(CurrentPointIndex) ? Points[CurrentPointIndex] : nullptr;
+}
---
+++ Source/Rapid/Public/AI/Components/PatrolComponent.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PatrolComponent.generated.h"
+
+
+UCLASS(ClassGroup=("AI"), meta=(BlueprintSpawnableComponent))
+class RAPID_API UPatrolComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPatrolComponent();
+
+	UFUNCTION(BlueprintPure)
+	void SetPointToNext();
+
+	UFUNCTION(BlueprintPure)
+	TSoftObjectPtr<USceneComponent> GetCurrentPoint() const;
+
+protected:
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Config")
+	TArray<TSoftObjectPtr<USceneComponent>> Points;
+
+private:
+	int32 CurrentPointIndex = INDEX_NONE;
+};
