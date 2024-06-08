1c5b5609aa58a80c37fb098e9566c29b04858735
apokrif6 <j.berenchan@gmail.com>
2024-06-08 17:04:24 +0200

    Create basic hud

---
+++ Source/Rapid/Public/UI/MainHUD.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "MainHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAPID_API AMainHUD : public AHUD
+{
+	GENERATED_BODY()
+};
