49c7396f156a487fa3af59954261bd1dbeb276fb
apokrif6 <j.berenchan@gmail.com>
2024-07-01 21:33:29 +0200

    Bind Health and Ammo to GetGameplayAttributeValueChangeDelegate. Change visual representation on hud

---
+++ Source/Rapid/Private/UI/HUDWidget.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/HUDWidget.h"
+#include "AbilitySystem/Attributes/RangeAttributeSet.h"
+
+void UHUDWidget::NativeConstruct()
+{
+	if (const APawn* OwningPawn = GetOwningPlayerPawn())
+	{
+		if (UAbilitySystemComponent* AbilitySystemComponent = OwningPawn->GetComponentByClass<
+			UAbilitySystemComponent>())
+		{
+			OnHealthAttributeChangedHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+				                                                         UBaseAttributeSet::GetHealthAttribute()).
+			                                                         AddUObject(this, &ThisClass::OnAttributeChanged);
+
+			OnAmmoAttributeChangedHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+				                                                       URangeAttributeSet::GetAmmoAttribute()).
+			                                                       AddUObject(this, &ThisClass::OnAttributeChanged);
+		}
+	}
+
+	Super::NativeConstruct();
+}
+
+void UHUDWidget::NativeDestruct()
+{
+	if (const APawn* OwningPawn = GetOwningPlayerPawn())
+	{
+		if (UAbilitySystemComponent* AbilitySystemComponent = OwningPawn->GetComponentByClass<
+			UAbilitySystemComponent>())
+		{
+			AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UBaseAttributeSet::GetHealthAttribute()).
+			                        Remove(OnHealthAttributeChangedHandle);
+
+			AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(URangeAttributeSet::GetAmmoAttribute()).
+			                        Remove(OnAmmoAttributeChangedHandle);
+		}
+	}
+
+	Super::NativeDestruct();
+}
+
+void UHUDWidget::OnAttributeChanged(const FOnAttributeChangeData& ChangeData)
+{
+	if (ChangeData.Attribute == UBaseAttributeSet::GetHealthAttribute())
+		OnHealthChanged(ChangeData.NewValue);
+
+	if (ChangeData.Attribute == URangeAttributeSet::GetAmmoAttribute())
+		OnAmmoChanged(ChangeData.NewValue);
+}
---
+++ Source/Rapid/Public/UI/HUDWidget.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Attributes/RangeAttributeSet.h"
+#include "Blueprint/UserWidget.h"
+#include "HUDWidget.generated.h"
+
+class UAbilityAsync_WaitAttributeChanged;
+/**
+ * 
+ */
+UCLASS()
+class RAPID_API UHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+
+	virtual void NativeDestruct() override;
+
+	void OnAttributeChanged(const FOnAttributeChangeData& ChangeData);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnHealthChanged(const float NewHealth);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnAmmoChanged(const float NewAmmo);
+
+private:
+	FDelegateHandle OnHealthAttributeChangedHandle;
+
+	FDelegateHandle OnAmmoAttributeChangedHandle;
+};
